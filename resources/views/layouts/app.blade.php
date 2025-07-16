e2c27c287c47f836abc153bc22af74b6484746f6
Osama Salem <os2492045@gmail.com>
2025-07-16 11:51:52 +0300

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta http-equiv="Content-Language" content="ar">
     <title>Phoenix Soul</title>
     {{-- Meta Tags للسيو --}}
